593b420f3114d21de29f85af58872e189409a920
Mahmoud Abdelfattah <abdelfattah10@yahoo.com>
2022-12-03 12:41:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const handleClick = async () => {
+    try {
+      const resp = await axios.get("https://theravr.onrender.com/");
+      console.log(resp.data);
+    } catch (e) {}
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <button onClick={handleClick}>Send request</button>
     </div>
   );
 }
